365fa36fd6173ad3a81def2866353244ff735e70
Artem Tokarevskikh <ArtemT@clearwayintegration.com>
2024-08-24 18:04:21 +0300

    Повторение пройденного.

--- Head First Python/ch01/odd.py
+++ Head First Python/ch01/odd.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import time
 import random
 
+print('Start program')
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
         41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
@@ -12,6 +13,7 @@ for i in range(5):
         print(f'Minutes {right_this_minute: 2d} is odd')
     else:
         print(f'Minutes {right_this_minute: 2d} is even')
-    rnd = random.randint(1, 60)
+    rnd = random.randint(1, 20)
     print(f'Sleep for {rnd: 2d} seconds')
     time.sleep(rnd)
+print('The end')
