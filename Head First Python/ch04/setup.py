e2ec4336471c697e925f6b4460d1e7f2b99ef980
Artem Tokarevskikh <ArtemT@clearwayintegration.com>
2025-07-26 21:03:20 +0300

    Create and install distributive for module.

---
+++ Head First Python/ch04/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The search tools (project for learning Python)',
+    author='Artem Tokarevskikh',
+    author_email='tokarevskikh@gmail.com',
+    url='https://github.com/atokarevskikh/learn_python',
+    py_modules=['vsearch'],
+)
