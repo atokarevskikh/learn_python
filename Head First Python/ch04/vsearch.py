9886e4b2cd249964c03e33e789bf8978aac56f74
Artem Tokarevskikh <ArtemT@clearwayintegration.com>
2025-07-26 12:02:29 +0300

    Add function search4letters.

--- Head First Python/ch04/vsearch.py
+++ Head First Python/ch04/vsearch.py
@@ -1,5 +1,13 @@
+
+
 def search4vowels(phrase:str) -> set:
     """Возвращает множество глассных, найденных в указанном слове или фразе"""
     vowels = set('aeoiu')
     return vowels.intersection(set(phrase.casefold()))
+
+
+def search4letters(phrase:str, letters:str = 'aeoiu') -> set:
+    """Возвращает множество букв (без учёта регистра),
+       найденных в переданной фразе."""
+    return set(letters.casefold()).intersection(set(phrase.casefold()))
 
